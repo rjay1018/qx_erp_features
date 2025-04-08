f706aa7a2a54201649b2132caf01751c9ae0d680
Rjay Lopez <rjay1018@gmail.com>
2025-04-08 21:55:19 +0800

    update xml
    xml update <?xml version="1.0" encoding="UTF-8" ?>

--- qx_inventory_reorder_forecast/__manifest__.py
+++ qx_inventory_reorder_forecast/__manifest__.py
@@ -7,11 +7,11 @@
     'category': 'Inventory',
     'depends': ['stock'],
     'data': [
-        'security/ir.model.access.csv',
-        'views/product_views.xml',
-        'views/product_kanban_forecast.xml',
-        'views/forecast_dashboard_views.xml',
-    ],
+            'security/ir.model.access.csv',
+            'views/product_views.xml',
+            'views/product_kanban_forecast.xml',
+            'views/forecast_dashboard_views.xml',
+        ],
     'installable': True,
     'application': False,
 }
