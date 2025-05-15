79583df6446defe00b1a666a07e43c7c5d559e84
Rjay Lopez <rjay1018@gmail.com>
2025-05-16 00:30:14 +0800

    fix wizards

---
+++ qx_employee_checklist/wizards/employee_checklist_document_wizard.py
@@ -0,0 +1,34 @@
+from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError
+
+class EmployeeChecklistDocumentWizard(models.TransientModel):
+    _name = 'employee.checklist.document.wizard'
+    _description = 'Checklist Document Upload Wizard'
+
+    employee_id = fields.Many2one('hr.employee', string="Employee", required=True)
+    checklist_item_id = fields.Many2one(
+        'employee.checklist',
+        string='Checklist Item',
+        required=True,
+        domain="[('document_type', 'in', ['entry', 'exit'])]",
+    )
+    attachment = fields.Binary(string="Document File", required=True)
+    attachment_filename = fields.Char("Filename")
+    description = fields.Text("Description")
+
+    def action_upload(self):
+        if not self.attachment:
+            raise ValidationError(_("Please upload a document."))
+
+        attachment = self.env['ir.attachment'].create({
+            'name': self.attachment_filename or self.checklist_item_id.name,
+            'datas': self.attachment,
+            'res_model': 'hr.employee.document',
+        })
+
+        self.env['hr.employee.document'].create({
+            'employee_ref': self.employee_id.id,
+            'doc_attachment_id': [(6, 0, [attachment.id])],
+            'document_name': self.checklist_item_id.id,
+            'description': self.description or self.checklist_item_id.name,
+        })
