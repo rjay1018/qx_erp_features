965a7720d15fcef29902a4fc2b9f904d8877fba9
Rjay Lopez <rjay1018@gmail.com>
2025-05-16 00:20:29 +0800

    Update __manifest__.py

--- qx_employee_checklist/__manifest__.py
+++ qx_employee_checklist/__manifest__.py
@@ -4,7 +4,7 @@
     'category': 'Human Resources',
     'summary': 'Upload checklist documents for employee entry/exit processes',
     'author': 'jaynatz', 
-    'depends': ['hr', 'hr_documents', 'oh_employee_check_list'],
+    'depends': ['hr','oh_employee_check_list'],
     'data': [
         'views/hr_employee_form_inherit.xml',
         'wizards/employee_checklist_document_wizard_view.xml',
