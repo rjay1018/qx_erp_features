943585b9b8e36b022130ef5be0ab561bb44d5ce6
Rjay Lopez <rjay1018@gmail.com>
2025-07-08 00:07:41 +0800

    Update hide_import_button.js

--- qx_hide_import_button/static/src/js/hide_import_button.js
+++ qx_hide_import_button/static/src/js/hide_import_button.js
@@ -1,4 +1,4 @@
-odoo.define('hide_import_button.hide_import_button', function (require) {
+odoo.define('qx_hide_import_button.hide_import_button', function (require) {
     "use strict";
 
     var ListView = require('web.ListView');
@@ -6,13 +6,18 @@ odoo.define('hide_import_button.hide_import_button', function (require) {
     ListView.include({
         renderButtons: function ($node) {
             this._super($node);
+
+            console.log("✅ JS Loaded: Checking user group...");
+
             var self = this;
             this._rpc({
                 model: 'res.users',
                 method: 'has_group',
-                args: ['hide_import_button.group_no_import'],
+                args: ['qx_hide_import_button.group_no_import'],
             }).then(function (inGroup) {
+                console.log("✅ User in restricted group?", inGroup);
                 if (inGroup && self.$buttons) {
+                    console.log("❌ Hiding Import Button");
                     self.$buttons.find('.o_list_button_import').remove();
                 }
             });
