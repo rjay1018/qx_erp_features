99a2c9f729dfb24494d71c9811bcaf7bba220a8c
Rjay Lopez <rjay1018@gmail.com>
2025-08-19 23:35:48 +0800

    Update product.py

--- qx_customer_barcode/models/product.py
+++ qx_customer_barcode/models/product.py
@@ -1,15 +1,31 @@
-from odoo import models
+# models/product_template.py
+from odoo import models, fields, api
 
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
+    customer_barcode_ids = fields.One2many(
+        'customer.product.barcode',
+        'product_tmpl_id',
+        string="Customer Barcodes"
+    )
+    customer_barcode_count = fields.Integer(
+        string="Customer Barcode Count",
+        compute="_compute_customer_barcode_count"
+    )
+
+    def _compute_customer_barcode_count(self):
+        for product in self:
+            product.customer_barcode_count = len(product.customer_barcode_ids)
+
     def action_open_customer_barcodes(self):
+        """Button action to open customer barcodes for this product."""
         self.ensure_one()
         return {
-            "name": "Customer Barcodes",
-            "type": "ir.actions.act_window",
-            "res_model": "customer.product.barcode",
-            "view_mode": "tree,form",
-            "domain": [("product_tmpl_id", "=", self.id)],
-            "context": {"default_product_tmpl_id": self.id},
+            'type': 'ir.actions.act_window',
+            'name': 'Customer Barcodes',
+            'res_model': 'customer.product.barcode',
+            'view_mode': 'tree,form',
+            'domain': [('product_tmpl_id', '=', self.id)],
+            'context': {'default_product_tmpl_id': self.id},
         }
